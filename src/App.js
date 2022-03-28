69984d0f513835bddadfdc2ffca718fe8c400492
Betsimisaraka <anita.ras@onja.org>
2022-03-28 09:49:59 +0300

    Modifying the perspective bar

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import React from 'react';
 
 import CharacterEditor from './components/CharacterEditor';
 import Footer from './components/Footer';
-import PerspectiveBar from './components/PerspectiveBar';
 
 function App() {
   return (
     <>
       <CharacterEditor />
-      <PerspectiveBar />
       <Footer />
     </>
   );
